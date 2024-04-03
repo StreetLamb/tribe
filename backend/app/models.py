10c516607e4474443b3b07b6da845076a13ed7ef
StreetLamb <l.y.z.jerron@gmail.com>
2024-04-03 21:36:57 +0800

    backend: define classes for api and to create tables in db

--- backend/app/models.py
+++ backend/app/models.py
@@ -45,6 +45,9 @@ class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner")
+    members: list["Member"] = Relationship(back_populates="owner")
+    projects: list["Project"] = Relationship(back_populates="owner")
+    skills: list["Skill"] = Relationship(back_populates="owner")
 
 
 # Properties to return via API, id is always required
@@ -111,3 +114,66 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str
+
+# ==== Member ====
+
+class MemberBase(SQLModel):
+    name: str
+    role: str
+    goal: str | None = None
+    backstory: str | None = None
+    
+class MemberCreate(MemberBase):
+    name: str
+    
+class MemberUpdate(MemberBase):
+    name: str | None = None
+    role: str | None = None
+
+class MemberSkillLink(SQLModel, table=True):
+    member_id: int | None = Field(default=None, foreign_key="skill.id", primary_key=True)
+    skill_id: int | None = Field(default=None, foreign_key="member.id", primary_key=True)
+    
+class Member(MemberBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int | None = Field(default=None, foreign_key="user.id")
+    owner: User | None = Relationship(back_populates="members")
+    skills: list["Skill"] = Relationship(back_populates="members", link_model=MemberSkillLink)
+    
+
+# ==== Project ====
+
+class ProjectBase(SQLModel):
+    name: str
+    description: str | None = None
+    structure: str | None = None
+
+class ProjectCreate(ProjectBase):
+    name: str
+
+class ProjectUpdate(ProjectBase):
+    name: str | None = None
+
+class Project(ProjectBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int | None = Field(default=None, foreign_key="user.id")
+    owner: User | None = Relationship(back_populates="projects")
+    
+# ==== Skill ====
+
+class SkillBase(SQLModel):
+    name: str
+    description: str | None = None
+
+class SkillCreate(SkillBase):
+    name: str
+
+class SkillUpdate(SkillBase):
+    name: str | None = None
+    description: str | None = None
+
+class Skill(SkillBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int | None = Field(default=None, foreign_key="user.id")
+    owner: User | None = Relationship(back_populates="skills")
+    members: list[Member] = Relationship(back_populates="skills", link_model=MemberSkillLink)
