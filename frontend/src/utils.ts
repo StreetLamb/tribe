e94c8bfaa6219076ef6a3d03c64e0b68f4fdd48c
StreetLamb <l.y.z.jerron@gmail.com>
2024-05-26 15:10:48 +0800

    Add util functions to convert checkpoint data to messages

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,4 +1,39 @@
+import type { Message, ToolInput } from "./components/Teams/ChatTeam"
+
 export const emailPattern = {
   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
   message: "Invalid email address",
+}
+
+interface CheckpointMessage {
+  kwargs: {
+    tool_calls?: ToolInput[]
+    name: string
+    type: string
+    content: string
+    next?: string
+  }
+}
+
+/**
+ * Convert langgraph's checkpoint data to messages
+ * @param checkpoint Checkpoint
+ */
+export const convertCheckpointToMessages = (checkpoint: any): Message[] => {
+  const messages: Message[] = []
+
+  for (const message of checkpoint.channel_values
+    .messages as CheckpointMessage[]) {
+    if (message.kwargs.type === "tool") continue
+    const { type, content, next, tool_calls, name } = message.kwargs
+    messages.push({
+      toolCalls: tool_calls || [],
+      member: type === "human" ? "You." : name,
+      type,
+      content,
+      next,
+    } as Message)
+  }
+
+  return messages
 }
