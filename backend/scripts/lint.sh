c0b2345ddf933381ecf9c9820bab5887641b925c
StreetLamb <l.y.z.jerron@gmail.com>
2024-06-17 08:59:21 +0800

    Try running mypy on a small portion of code

--- backend/scripts/lint.sh
+++ backend/scripts/lint.sh
@@ -9,26 +9,10 @@ check_memory_usage() {
   free -m
 }
 
-check_memory_usage "start"
-
 # Run mypy on app/core and check memory usage
-check_memory_usage "before mypy app/core"
-mypy app/core
-check_memory_usage "after mypy app/core"
+check_memory_usage "before"
+mypy app/core/graph/skills
+check_memory_usage "after"
 
-# Run mypy on app/api and check memory usage
-check_memory_usage "before mypy app/api"
-mypy app/api
-check_memory_usage "after mypy app/api"
-
-# Run ruff linter and check memory usage
-check_memory_usage "before ruff"
 ruff app
-check_memory_usage "after ruff"
-
-# Run ruff formatter and check memory usage
-check_memory_usage "before ruff format"
 ruff format app --check
-check_memory_usage "after ruff format"
-
-check_memory_usage "end"
