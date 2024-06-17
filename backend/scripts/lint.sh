1a2bda260c8e4f24c08cf163f9b78f163a4079b2
StreetLamb <l.y.z.jerron@gmail.com>
2024-06-17 10:10:32 +0800

    Revert linting stage to original and remove swap space from github test action.

--- backend/scripts/lint.sh
+++ backend/scripts/lint.sh
@@ -3,16 +3,6 @@
 set -e
 set -x
 
-# Function to check memory usage
-check_memory_usage() {
-  echo "Memory usage at $1:"
-  free -m
-}
-
-# Run mypy on app/core and check memory usage
-check_memory_usage "before"
-mypy app/core/graph/skills --show-traceback
-check_memory_usage "after"
-
+mypy app
 ruff app
 ruff format app --check
