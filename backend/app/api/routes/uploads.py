1a2ee813e4f90b1610fa93708ecc2739cbc61085
StreetLamb <l.y.z.jerron@gmail.com>
2024-06-23 08:38:30 +0800

    Fix create_upload by committing upload to db first to get upload id. Simplify error handling for create_upload and update_upload routes.

--- backend/app/api/routes/uploads.py
+++ backend/app/api/routes/uploads.py
@@ -99,31 +99,25 @@ def create_upload(
     file_size: int = Depends(valid_content_length),
 ) -> Any:
     """Create upload"""
-
     if file.content_type not in ["application/pdf"]:
         raise HTTPException(status_code=400, detail="Invalid document type")
 
-    try:
-        temp_file = save_file_if_within_size_limit(file, file_size)
-        upload = Upload.model_validate(
-            UploadCreate(name=name, description=description),
-            update={"owner_id": current_user.id},
-        )
-        session.add(upload)
+    temp_file = save_file_if_within_size_limit(file, file_size)
+    upload = Upload.model_validate(
+        UploadCreate(name=name, description=description),
+        update={"owner_id": current_user.id},
+    )
+    session.add(upload)
+    session.commit()
 
-        # To appease type-checking. This should never happen.
-        if current_user.id is None or upload.id is None:
-            raise HTTPException(
-                status_code=500, detail="Failed to retrieve user and upload ID"
-            )
-        QdrantStore().create(
-            temp_file.name, upload.id, current_user.id, chunk_size, chunk_overlap
+    # To appease type-checking. This should never happen.
+    if current_user.id is None or upload.id is None:
+        raise HTTPException(
+            status_code=500, detail="Failed to retrieve user and upload ID"
         )
-
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail="Internal Server Error") from e
+    QdrantStore().create(
+        temp_file.name, upload.id, current_user.id, chunk_size, chunk_overlap
+    )
 
     return upload
 
@@ -147,42 +141,35 @@ def update_upload(
     if not current_user.is_superuser and upload.owner_id != current_user.id:
         raise HTTPException(status_code=403, detail="Not enough permissions")
 
-    try:
-        update_data: dict[str, str | datetime] = {}
-        if name is not None:
-            update_data["name"] = name
-        if description is not None:
-            update_data["description"] = description
-        if update_data:
-            update_data["last_modified"] = datetime.now()
-            update_dict = UploadUpdate(**update_data).model_dump(exclude_unset=True)
-            upload.sqlmodel_update(update_dict)
-            session.add(upload)
+    update_data: dict[str, str | datetime] = {}
+    if name is not None:
+        update_data["name"] = name
+    if description is not None:
+        update_data["description"] = description
+    if update_data:
+        update_data["last_modified"] = datetime.now()
+        update_dict = UploadUpdate(**update_data).model_dump(exclude_unset=True)
+        upload.sqlmodel_update(update_dict)
+        session.add(upload)
 
-        if file:
-            if file.content_type not in ["application/pdf"]:
-                raise HTTPException(status_code=400, detail="Invalid document type")
-            temp_file = save_file_if_within_size_limit(file, file_size)
-            if upload.owner_id is None:
-                raise HTTPException(
-                    status_code=500, detail="Failed to retrieve owner ID"
-                )
-            if chunk_overlap is None or chunk_size is None:
-                raise HTTPException(
-                    status_code=400,
-                    detail="If file is provided, chunk size and chunk overlap must be provided.",
-                )
-            QdrantStore().delete(id, upload.owner_id)
-            QdrantStore().create(
-                temp_file.name, id, upload.owner_id, chunk_size, chunk_overlap
+    if file:
+        if file.content_type not in ["application/pdf"]:
+            raise HTTPException(status_code=400, detail="Invalid document type")
+        temp_file = save_file_if_within_size_limit(file, file_size)
+        if upload.owner_id is None:
+            raise HTTPException(status_code=500, detail="Failed to retrieve owner ID")
+        if chunk_overlap is None or chunk_size is None:
+            raise HTTPException(
+                status_code=400,
+                detail="If file is provided, chunk size and chunk overlap must be provided.",
             )
+        QdrantStore().delete(id, upload.owner_id)
+        QdrantStore().create(
+            temp_file.name, id, upload.owner_id, chunk_size, chunk_overlap
+        )
 
-        session.commit()
-        session.refresh(upload)
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail="Internal Server Error") from e
-
+    session.commit()
+    session.refresh(upload)
     return upload
 
 
