198e31238dd73719f9d5b9aa3efc0d516b44873e
StreetLamb <l.y.z.jerron@gmail.com>
2024-07-31 22:08:02 +0800

    Update frontend client types

---
+++ frontend/src/client/models/Interrupt.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { InterruptDecision } from './InterruptDecision';
+
+export type Interrupt = {
+    decision: InterruptDecision;
+    rejection_message?: (string | null);
+};
+
